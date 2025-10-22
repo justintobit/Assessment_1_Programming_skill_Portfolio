8310ca3a7c95c7480b5f838fe173b1dbd1d731ea
justintobit <jtjacobe@gmail.com>
2025-10-22 14:12:33 +0400

    Add files via upload

--- EX 1.py
+++ EX 1.py
@@ -3,10 +3,10 @@ Coding is Cool
 Ex.1
 """
 
-
-word1 =("Coding ")
+#assigns "Coding-is-Cool" each word to a variable
+word1 =("Coding")
 word2 =("is ")
 word3 =("Cool ")
 
-# Use string concatenation to combine the variables and print the phrase
+# prints the word from the assigned variable
 print(word1+word2+word3)
