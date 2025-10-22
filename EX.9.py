541ac7f461962a4ea36d84589b4a4c33997a517f
justintobit <jtjacobe@gmail.com>
2025-10-22 14:37:29 +0400

    Add files via upload

--- EX.9.py
+++ EX.9.py
@@ -7,10 +7,10 @@ Hello - 10 Marks
 
 
 def hello():
-    print("Hello")  # Fill in this blank to print "Hello" to the console
+    print("Hello")  # Fill to print "Hello" to the console
 
 def main():
-   print("Hello")  # Fill in this blank to call the hello() function
+   print("Hello")  # to call the hello() function
 hello()
 
 if __name__ == "__main__":
